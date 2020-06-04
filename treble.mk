580936df75910d885d6130a24ecb1a208e8323fd
mfshujath <mmfshujath@gmail.com>
2020-06-04 12:21:48 +0530

    remove secure element

--- treble.mk
+++ treble.mk
@@ -82,10 +82,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
 
-# NFC - SECURE ELEMENT
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0-service \
-    SecureElement
 
 # POWER
 PRODUCT_PACKAGES += \
